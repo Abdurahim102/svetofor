b24b26569566ff3e2753debe4a7ed34b4965e139
Abdurahim102 <abundance0121@gmail.com>
2024-02-26 16:57:55 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let red1 = document.querySelector('.red')
+
+let yellow = document.querySelector('.yellow')
+let green = document.querySelector('.green')
+
+
+
+
+
+setInterval(() => {
+    setTimeout(() => {
+        red1.style.background = "red"
+    }, 2000);   
+    
+    setTimeout(() => {
+        red1.style.background = "black"
+    }, 6000);
+    
+    setTimeout(() => {
+        yellow.style.background = "yellow"
+    }, 4000);
+    setTimeout(() => {
+        yellow.style.background = "black"
+    }, 6000);
+    
+    setTimeout(() => {
+        green.style.background = "green"
+    
+    }, 6000)
+    setTimeout(() => {
+        green.style.background = "black"
+    
+    }, 7000)
+    
+}, 6000);
